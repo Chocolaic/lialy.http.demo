6e2d586e597b70080eac1cf277e13b154b4b33b2
Lialy <chocolate_liy@163.com>
2019-01-03 00:03:33 +0800

    up

--- src/Loader.js
+++ src/Loader.js
@@ -17,7 +17,12 @@ var loader = new resLoader({
 		$('.progresstext .total').text(total);
 	},
 	onComplete : function(total){
-		window.location.href = "live2d.html?"+getViewCode();
+		if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)))
+		{
+			window.location.href = "m_live2d.html?"+getViewCode();
+		}else{
+			window.location.href = "live2d.html?"+getViewCode();
+		}
 	}
 });
 
