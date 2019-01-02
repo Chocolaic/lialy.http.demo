ad5fbc2a915b50fb3da27ceb6989a0bdf4816acc
Lialy <chocolate_liy@163.com>
2019-01-03 00:45:19 +0800

    up

--- src/view.js
+++ src/view.js
@@ -1,7 +1,7 @@
 function getViewCode()
 {
 	var date= new Date();
-	var time=date.getMonth()+date.getDate()+date.getHours()+date.getMinutes();
+	var time=date.getMonth()+date.getDate()+date.getHours();
 	var addr=returnCitySN['cip'];
 	var code=hex_md5(encodeURIComponent(time+addr));
 	return code;
